775d3937615916ef3570b00c05b805de07af34ea
DevJIYUL <lnacles@naver.com>
2022-04-13 03:13:46 +0900

    json serializer

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,3 +1,12 @@
+
+from django.urls import include,path
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register('post',views.PostViewSet) # 2개 URL를 만듬
+
 urlpatterns = [
-    
+    path("public/", views.PublicPostListAPIView.as_view()),
+    path('',include(router.urls)),
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,24 @@
+from rest_framework import generics
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import PostSerializer
+from .models import Post
 
-# Create your views here.
+class PublicPostListAPIView(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def dispatch(self,request,*args,**kwargs):
+
+        return super().dispatch(request,*args,**kwargs)
+# def post_list(request):
+#     pass
+
+# def post_detail(request):
+#     pass
